9f27db8f316275e4571869dacc112e3483e3add5
ZhouJi <263074289@qq.com>
2017-12-19 13:37:56 +0800

    Fast method of the Inverse Squred Root

---
+++ cpp/FastInvSqrt.cpp
@@ -0,0 +1,10 @@
+float FastInvSqrt(float x)
+{
+    float xhalf = 0.5f * x;
+    int i = *(int*)&x;
+    i = 0x5f3759df - (i>>1);
+    x = *(float*)&i;
+    x = x * (1.5f-(xhalf*x*x));
+    x = x * (1.5f-(xhalf*x*x));
+    return x;
+}
