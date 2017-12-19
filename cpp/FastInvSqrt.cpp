aa06aa97ce3bd4ece2a3a0486b312f83589acef7
ZhouJi <263074289@qq.com>
2017-12-19 13:59:18 +0800

    comments on Newton method

--- cpp/FastInvSqrt.cpp
+++ cpp/FastInvSqrt.cpp
@@ -4,6 +4,8 @@ float FastInvSqrt(float x)
     int i = *(int*)&x;
     i = 0x5f3759df - (i>>1);
     x = *(float*)&i;
+
+    //two times of Newton method to improve the precision
     x = x * (1.5f-(xhalf*x*x));
     x = x * (1.5f-(xhalf*x*x));
     return x;
